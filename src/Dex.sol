b422cf80bfadcc560fd4639ba952ab514dd7d0e8
choihs0457 <choihs0457@gmail.com>
2024-08-24 12:44:34 +0900

    init

---
+++ src/Dex.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Dex {
+    uint256 public number;
+
+    constructor(address address1_, address address2_) {
+        
+    }
+
+    function addLiquidity(uint256 amountX, uint256 amountY, uint256 minLPReturn) public returns (uint256) {
+        
+    }
+
+    function removeLiquidity(uint256 lpAmount, uint256 minAmountX, uint256 minAmountY) public returns (uint256, uint256) {
+        
+    }
+
+    function swap(uint256 amountXIn, uint256 amountYIn, uint256 minAmountOut) public returns (uint256) {
+        
+    }
+}
